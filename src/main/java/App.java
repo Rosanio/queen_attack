e69240b3bc5d07e13c81723eec92e332fc7e5552
Anna Becker and Matt Rosanio <anna.r.becker@gmail.com>
2016-02-08 11:55:46 -0800

    Add passing test for when queen can attack vertically.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -6,7 +7,12 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) {}
 
+  public static Boolean queenCanAttack(ArrayList queenLocation, ArrayList targetLocation) {
+    if(queenLocation.get(0) == targetLocation.get(0)) {
+      return true;
+    }
+    return false;
   }
 }
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,30 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+public class AppTest {
+  @Test
+  public void queenCanAttack_isFalseIftheQueenCannotAttack_false() {
+    App queenAttack = new App();
+    ArrayList<Object> queenLocation = new ArrayList<Object>();
+    queenLocation.add(1);
+    queenLocation.add(1);
+    ArrayList<Object> targetLocation = new ArrayList<Object>();
+    targetLocation.add(2);
+    targetLocation.add(3);
+    assertEquals(false, queenAttack.queenCanAttack(queenLocation, targetLocation));
+  }
+
+  @Test
+  public void queenCanAttack_isTrueIftheQueenCanAttackVertically_true() {
+    App queenAttack = new App();
+    ArrayList<Object> queenLocation = new ArrayList<Object>();
+    queenLocation.add(1);
+    queenLocation.add(1);
+    ArrayList<Object> targetLocation = new ArrayList<Object>();
+    targetLocation.add(1);
+    targetLocation.add(3);
+    assertEquals(true, queenAttack.queenCanAttack(queenLocation, targetLocation));
+  }
+}

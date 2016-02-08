600e8e4d74ab82cfb3020129a946e08525807306
Anna Becker and Matt Rosanio <anna.r.becker@gmail.com>
2016-02-08 13:21:37 -0800

    Add test to see if queen can attack diagonally

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,10 +9,14 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {}
 
-  public static Boolean queenCanAttack(ArrayList queenLocation, ArrayList targetLocation) {
+  public static Boolean queenCanAttack(ArrayList<Integer> queenLocation, ArrayList<Integer> targetLocation) {
     if(queenLocation.get(0) == targetLocation.get(0)) {
       return true;
     } else if (queenLocation.get(1) == targetLocation.get(1)) {
+      return true;
+    }
+    Integer slope = (targetLocation.get(1) - queenLocation.get(1)) / (targetLocation.get(0) - queenLocation.get(0));
+    if (slope == 1 || slope == -1) {
       return true;
     }
     return false;
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,10 +7,10 @@ public class AppTest {
   @Test
   public void queenCanAttack_isFalseIftheQueenCannotAttack_false() {
     App queenAttack = new App();
-    ArrayList<Object> queenLocation = new ArrayList<Object>();
+    ArrayList<Integer> queenLocation = new ArrayList<Integer>();
     queenLocation.add(1);
     queenLocation.add(1);
-    ArrayList<Object> targetLocation = new ArrayList<Object>();
+    ArrayList<Integer> targetLocation = new ArrayList<Integer>();
     targetLocation.add(2);
     targetLocation.add(3);
     assertEquals(false, queenAttack.queenCanAttack(queenLocation, targetLocation));
@@ -19,10 +19,10 @@ public class AppTest {
   @Test
   public void queenCanAttack_isTrueIftheQueenCanAttackVertically_true() {
     App queenAttack = new App();
-    ArrayList<Object> queenLocation = new ArrayList<Object>();
+    ArrayList<Integer> queenLocation = new ArrayList<Integer>();
     queenLocation.add(1);
     queenLocation.add(1);
-    ArrayList<Object> targetLocation = new ArrayList<Object>();
+    ArrayList<Integer> targetLocation = new ArrayList<Integer>();
     targetLocation.add(1);
     targetLocation.add(3);
     assertEquals(true, queenAttack.queenCanAttack(queenLocation, targetLocation));
@@ -31,12 +31,24 @@ public class AppTest {
   @Test
   public void queenCanAttack_isTrueIftheQueenCanAttackHorizontally_true() {
     App queenAttack = new App();
-    ArrayList<Object> queenLocation = new ArrayList<Object>();
+    ArrayList<Integer> queenLocation = new ArrayList<Integer>();
     queenLocation.add(1);
     queenLocation.add(1);
-    ArrayList<Object> targetLocation = new ArrayList<Object>();
+    ArrayList<Integer> targetLocation = new ArrayList<Integer>();
     targetLocation.add(4);
     targetLocation.add(1);
+    assertEquals(true, queenAttack.queenCanAttack(queenLocation, targetLocation));
+  }
+
+  @Test
+  public void queenCanAttack_isTrueIftheQueenCanAttackDiagonally_true() {
+    App queenAttack = new App();
+    ArrayList<Integer> queenLocation = new ArrayList<Integer>();
+    queenLocation.add(4);
+    queenLocation.add(4);
+    ArrayList<Integer> targetLocation = new ArrayList<Integer>();
+    targetLocation.add(6);
+    targetLocation.add(2);
     assertEquals(true, queenAttack.queenCanAttack(queenLocation, targetLocation));
   }
 }
